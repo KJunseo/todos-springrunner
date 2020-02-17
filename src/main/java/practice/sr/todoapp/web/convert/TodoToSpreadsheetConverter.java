bfe25858ae11b4bf350c3f8de87424f73c5a8ce6
KJunseo <max9106@naver.com>
2020-02-18 02:09:16 +0900

    웹 모듈 패키지

---
+++ src/main/java/practice/sr/todoapp/web/convert/TodoToSpreadsheetConverter.java
@@ -0,0 +1,37 @@
+package practice.sr.todoapp.web.convert;
+
+import practice.sr.todoapp.commons.domain.Spreadsheet;
+import practice.sr.todoapp.commons.util.StreamUtils;
+import practice.sr.todoapp.core.todos.domain.Todo;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * {@link Todo} 모델을 {@link Spreadsheet} 모델로 변환하는 변환기
+ *
+ * @author springrunner.kr@gmail.com
+ */
+public class TodoToSpreadsheetConverter {
+
+    public Spreadsheet convert(Iterable<Todo> todos) {
+        Spreadsheet.Row header = new Spreadsheet.Row()
+                                                .addCell("id")
+                                                .addCell("title")
+                                                .addCell("completed");
+
+        Stream<Todo> todoStream = StreamUtils.createStreamFromIterator(todos.iterator());
+        List<Spreadsheet.Row> rows = todoStream.map(this::mapRow).collect(Collectors.toList());
+
+        return new Spreadsheet("todos", header, rows);
+    }
+
+    private Spreadsheet.Row mapRow(Todo todo) {
+        return new Spreadsheet.Row()
+                              .addCell(todo.getId())
+                              .addCell(todo.getTitle())
+                              .addCell(todo.isCompleted() ? "완료" : "미완료");
+    }
+
+}
