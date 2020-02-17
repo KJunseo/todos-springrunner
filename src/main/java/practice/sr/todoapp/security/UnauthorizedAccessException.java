11425d271ddfaba6cd5bd2d0a0e79bb789b1a510
KJunseo <max9106@naver.com>
2020-02-18 02:08:45 +0900

    보안 모듈 패키지

---
+++ src/main/java/practice/sr/todoapp/security/UnauthorizedAccessException.java
@@ -0,0 +1,16 @@
+package practice.sr.todoapp.security;
+
+import practice.sr.todoapp.commons.SystemException;
+
+/**
+ * 인증되지 않은 사용자 접근시 발생 가능한 예외 클래스
+ *
+ * @author springrunner.kr@gmail.com
+ */
+public class UnauthorizedAccessException extends SystemException {
+
+    public UnauthorizedAccessException() {
+        super("인증되지 않은 접근입니다.");
+    }
+
+}
