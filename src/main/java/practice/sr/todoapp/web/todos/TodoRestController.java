f7d506302c99f0ec0e5c47913ac5194116f01f04
KJunseo <max9106@naver.com>
2020-02-20 21:29:30 +0900

    할 일 등록 & 최소 4자 이상 조건주기

--- src/main/java/practice/sr/todoapp/web/todos/TodoRestController.java
+++ src/main/java/practice/sr/todoapp/web/todos/TodoRestController.java
@@ -1,23 +1,64 @@
 package practice.sr.todoapp.web.todos;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import practice.sr.todoapp.core.todos.application.TodoEditor;
 import practice.sr.todoapp.core.todos.application.TodoFinder;
 import practice.sr.todoapp.core.todos.domain.Todo;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @RestController
 public class TodoRestController {
 
-    private TodoFinder todoFinder;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    public TodoRestController(TodoFinder todoFinder) {
+    private TodoFinder todoFinder;
+    private TodoEditor todoEditor;
+
+    public TodoRestController(TodoFinder todoFinder, TodoEditor todoEditor) {
         this.todoFinder = todoFinder;
+        this.todoEditor = todoEditor;
     }
 
     @GetMapping("/api/todos")
     public List<Todo> list() {
         return todoFinder.getAll();
+    }
+
+    @PostMapping("/api/todos")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void create(@RequestBody @Valid TodoWriteCommand command) {
+        todoEditor.create(command.getTitle());
+
+        log.info("command: {}", command.getTitle());
+    }
+
+    static class TodoWriteCommand {
+
+        @Size(min = 4, max = 100)
+        private String title;
+
+        private boolean completed;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public boolean isCompleted() {
+            return completed;
+        }
+
+        public void setCompleted(boolean completed) {
+            this.completed = completed;
+        }
     }
 }
