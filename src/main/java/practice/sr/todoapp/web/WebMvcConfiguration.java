13a57284251e30ae1008ad14dca701fa3c3e837a
KJunseo <max9106@naver.com>
2020-02-23 13:05:44 +0900

    csv로 할 일 목록 다운로드

--- src/main/java/practice/sr/todoapp/web/WebMvcConfiguration.java
+++ src/main/java/practice/sr/todoapp/web/WebMvcConfiguration.java
@@ -1,10 +1,16 @@
 package practice.sr.todoapp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import practice.sr.todoapp.commons.web.view.CommaSeparatedValuesView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Spring Web MVC 설정
@@ -33,10 +39,15 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     /**
      * 스프링부트가 생성한 ContentNegotiatingViewResolver를 조작할 목적으로 작성된 컴포넌트
      */
+    @Configuration
     public static class ContentNegotiationCustomizer {
 
+        // 기존 view들에 새로 만든 view를 추가하여 viewResolver에 등록.
+        @Autowired
         public void configurer(ContentNegotiatingViewResolver viewResolver) {
-            // TODO ContentNegotiatingViewResolver 사용자 정의
+            List<View> defaultViews = new ArrayList<>(viewResolver.getDefaultViews());
+            defaultViews.add(new CommaSeparatedValuesView());
+            viewResolver.setDefaultViews(defaultViews);
         }
 
     }
--- src/main/java/practice/sr/todoapp/web/todos/TodoController.java
+++ src/main/java/practice/sr/todoapp/web/todos/TodoController.java
@@ -3,15 +3,24 @@ package practice.sr.todoapp.web.todos;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import practice.sr.todoapp.commons.domain.Spreadsheet;
+import practice.sr.todoapp.core.todos.application.TodoFinder;
+import practice.sr.todoapp.core.todos.domain.Todo;
+import practice.sr.todoapp.web.convert.TodoToSpreadsheetConverter;
 import practice.sr.todoapp.web.model.SiteProperties;
+
+import java.util.List;
 
 @Controller
 public class TodoController {
 
     private SiteProperties siteProperties;
 
-    public TodoController(SiteProperties siteProperties) {
+    private TodoFinder todoFinder;
+
+    public TodoController(SiteProperties siteProperties, TodoFinder todoFinder) {
         this.siteProperties = siteProperties; // 생성자를 통한 의존성 주입
+        this.todoFinder = todoFinder;
     }
 
     @RequestMapping("/todos")
@@ -21,6 +30,17 @@ public class TodoController {
                                  // 접미사: .html
                                  // 따라서 setViewName을 todos로 주면, templates에 있는 todos.html을 뜻함
         mv.addObject("site", siteProperties);
+        return mv;
+    }
+
+    @RequestMapping(value = "/todos", produces = "text/csv")
+    public ModelAndView downloadTodos() {
+        List<Todo> todos = todoFinder.getAll();
+        Spreadsheet spreadsheet = new TodoToSpreadsheetConverter().convert(todos);
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject(spreadsheet);
+
         return mv;
     }
 }
